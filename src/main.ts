a9465ced4672f00256e3c88917129586f3113981
SAK74 <sergijks@gmail.com>
2023-08-26 20:17:48 +0200

    init: setup with webpack builder

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import "./some";
+import "./input.css";
+
+console.log("Hello enviroument N1!");
+console.log("next line ");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{ts,js}"],
   theme: {
     extend: {
       colors: { custom: colors.amber },
@@ -23,4 +26,5 @@ module.exports = {
     },
   },
   plugins: [],
+  darkMode: "class",
 };
