a860b58da632bf097fe0a091b153270dc90efc8d
SAK74 <sergijks@gmail.com>
2023-09-23 19:38:33 +0200

    feat: add internal fonts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./setLib";
 import "./input.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/inter";
 
 console.log("Hello enviroument N1!");
 console.log("next line ");
