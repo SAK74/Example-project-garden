ae238241cb92914b3dd24f347fb81f623616c78e
SAK74 <sergijks@gmail.com>
2023-10-05 20:08:50 +0200

    feat: inititial popup with swiper

--- src/popup.ts
+++ src/popup.ts
@@ -1,12 +1,18 @@
 import "./popup.css";
-
-const gallery = document.getElementById("gallery");
+import "swiper/css";
+import "swiper/css/navigation";
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
 
 const handleClose = (ev: Event) => {
-  ev.stopPropagation();
-  console.log(ev.target);
+  // console.log("target: ", ev.target);
   const bg = document.getElementById("popup");
-  if (ev.target === bg) bg?.classList.add("hidden");
+  if (ev.target === ev.currentTarget) {
+    bg?.classList.add("hidden");
+    bg?.querySelectorAll(".swiper-slide").forEach((item) => {
+      item.remove();
+    });
+  }
 };
 
 const bgClick: EventListener = (ev) => {
@@ -15,21 +21,52 @@ const bgClick: EventListener = (ev) => {
   ) as HTMLCollectionOf<HTMLImageElement>;
 
   const background = document.getElementById("popup");
-  console.log(background);
+  // console.log(background);
   if (!background) {
     throw new Error("Cannot find the popup");
   }
-  background.classList.toggle("hidden");
+  background.classList.remove("hidden");
   const close = document.getElementById("popup-close");
 
-  const popupImg = new Image();
-  popupImg.src = photos[0].src;
-  popupImg.classList.add("popup-image");
-  background.appendChild(popupImg);
   background.addEventListener("click", handleClose);
+
+  const swiperWrapper = background.querySelector(".swiper-wrapper");
+  for (let i = 0; i < photos.length; i += 1) {
+    const item = new Image();
+    item.src = photos[i].src;
+    item.classList.add("swiper-slide");
+    console.log("load: ", item);
+    swiperWrapper?.appendChild(item);
+  }
+  // const swiper = new Swiper(".swiper", {
+  //   modules: [Navigation],
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   loop: true,
+
+  //   // autoplay: {
+  //   //   delay: 1000,
+  //   // },
+  //   centeredSlides: true,
+  // });
 };
 
 export const popup = () => {
   const bg = document.getElementById("photo-bg");
   bg?.addEventListener("click", bgClick);
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    // autoHeight: true,
+    // autoplay: {
+    //   delay: 1000,
+    // },
+    // centeredSlides: true,
+  });
 };
